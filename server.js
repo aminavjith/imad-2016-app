9b2f50818e6bdcc7e5d850c68c778bfa949252fd
aminavjith <amritha.n@gmail.com>
2016-09-27 14:37:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var Articles={
+var articles={
     'article-one':{
         title:'Article One',
         heading:'Article One',
@@ -71,7 +71,7 @@ app.get('/', function (req, res) {
 });
 app.get('/:articleName', function(req, res){
     var articleName= req.params.articleName;
-    res.send(createTemplate(Articles(articleName)));
+    res.send(createTemplate(articles(articleName)));
 });
 app.get('/article-two', function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
