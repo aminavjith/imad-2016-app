b9471f811941369c1383964663d2eda74293cbac
aminavjith <amritha.n@gmail.com>
2016-09-28 23:23:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+alert('From main.js.');
+
+var element = document.getElementById('text1');
+element.innerHTML = 'Amritha Navjith';
+
+var move = document.getElementById('img1');
+move.onclick = function() {
+    move.style.marginLeft = '100px';
+};
+
