261c89c579103c0b00981c692d93b80f32a4735b
aminavjith <amritha.n@gmail.com>
2016-09-29 00:09:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,8 +9,11 @@ var img = document.getElementById('img1');
 
 var marginLeft=0;
 function moveRight(){
+ do{
+     
   marginLeft = marginLeft + 1;
   img.style.marginLeft = marginLeft + 'px';
+ }while (marginLeft=='300px');
 
 }
 img.onclick = function(){
