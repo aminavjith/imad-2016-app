827aa0b6cfa37e8a1885c71d0c2c3933b8df5426
aminavjith <amritha.n@gmail.com>
2016-09-29 20:46:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,13 +19,21 @@ var inputName = document.getElementById('name');
 var name1 = inputName.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-    for (var i = 0; i < names.length; i++ ){
-        list += '<li>' + names[i] + '<li>';
-    }
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE)
+            if (request.status === 200)
+            {
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for (var i = 0; i < names.length; i++ ){
+                    list += '<li>' + names[i] + '<li>';
+                }
     var ul = document.getElementById('listing');
     ul.innerHTML = list;
+    request.open('GET','http://aminavjith.imad.hasura-app.io/submit-name?name=' + name1, true);
+    request.send(null);
 };
 
 
