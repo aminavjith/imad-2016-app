e24b758b127b5fa7e73aac72e3445cb3991e915a
aminavjith <amritha.n@gmail.com>
2016-09-29 19:36:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,14 +17,15 @@ button.onclick = function() {
 };
 var inputName = document.getElementById('name');
 var name1 = inputName.value;
-var submit = document.getElementById('submit1');
+var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     var names = ['name1', 'name2', 'name3', 'name4'];
     var list = '';
-    for(var i = 0; i < names.length; i++ ){
+    for (var i = 0; i < names.length; i++ ){
         list += '<li>' + names[i] + '<li>';
     }
     var ul = document.getElementById('listing');
+    ul.innerHTML = list;
 };
 
 
