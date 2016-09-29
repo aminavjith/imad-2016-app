002b0ba52f0cb43a6e562b86a32be838e4929c86
aminavjith <amritha.n@gmail.com>
2016-09-29 13:34:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,10 +2,20 @@
 var button = document.getElementById('button1');
 var counter = 0;
 button.onclick = function() {
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-}
+    var request = new XMLHttpRequest();
+    request.onReadyStateChange = function(){
+        if(request.readyState === XMLHttpRequest.DONE)
+            if (request.status === 200)
+            {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+    };
+    request.open('GET','http://aminavjith.imad.hasura-app.io/counter', true);
+    request.send(null);
+    
+};
 
 
 
