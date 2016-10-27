ded386df728ff95fea32b820ddf14e2ba0d4991d
aminavjith <amritha.n@gmail.com>
2016-10-27 13:31:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ app.get('/submit-name/', function(req, res){
 
 app.get('articles/:articleName', function(req, res){
     var articleName= req.params.articleName;
-    pool.query('SELECT * FROM article WHERE title = ' + articleName, function(err, result){
+    pool.query("SELECT * FROM article WHERE title = " + articleName + "'", function(err, result){
         if (err) {
             res.status(500).send(err.toString());
         } else if (result.rows.length === 0){
