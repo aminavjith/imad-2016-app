4b48e8e69a2b4dbe72ed80820bc9e8e295f3c484
aminavjith <amritha.n@gmail.com>
2016-10-27 13:41:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,13 +124,13 @@ app.get('/submit-name/', function(req, res){
 
 app.get('/articles/:articleName', function(req, res){
     var articleName= req.params.articleName;
-    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName + "'", function(err, result){
+    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err, result){
         if (err) {
             res.status(500).send(err.toString());
         } else if (result.rows.length === 0){
             res.status(404).send('File Not Found');
         } else {
-            var articleData = result.rows [0];
+            var articleData = result.rows[0];
             res.send(createTemplate(articleData));
         }
     });
