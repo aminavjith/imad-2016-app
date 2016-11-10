03fd155d2a75fd36a6d0f12d3ed517435397db5c
aminavjith <amritha.n@gmail.com>
2016-11-11 00:16:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -48,10 +48,10 @@ function createTemplate(data){
        <hr>
       </div>
       <textarea type="text" placeholder="Enter your comment here." id="comment" cols="50" rows="5"></textarea><br>
-      <input type="text" id="email" placeholder="Email address"/><br><br>
       <input type="Submit" id="submit_btn2"/>
       <ul id="listing">
       </ul>
+      
       <script type="text/javascript" src="/ui/main.js">
       </script>
      </html>`;
