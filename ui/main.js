795157a30df020edd98bb734b0964d1e627e25c2
aminavjith <amritha.n@gmail.com>
2016-11-10 13:18:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,22 +5,28 @@ submit.onclick = function() {
     var name1 = inputName.value;
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE)
+        if(request.readyState === XMLHttpRequest.DONE){
             if (request.status === 200)
             {
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list = '';
-                for (var i = 0; i < names.length; i++ ){
-                    list += '<li>' + names[i] + '</li>';
-                }
-            var ul = document.getElementById('listing');
-            ul.innerHTML = list;
+               console.log('User logged in.');
+               alert('Logged in successfully.');
             }
+            else if(request.status === 403)
+            {
+                alert('Incorrect credentials.');
+            }
+            else if(request.status === 500)
+            {
+                alert('Unknown error');
+            }
+        }
     };
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
     request.open('POST','http://aminavjith.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({username: username, password: password}));
 };
 
