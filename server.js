7cc8fc243fec526e486f7c6750c3e1f93f686293
aminavjith <amritha.n@gmail.com>
2016-11-12 22:57:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get('/register/', function(req, res){
    
     var salt = crypto.randomBytes(128).toString('hex');
     var dbString = hash(password, salt);
-    pool.query('INSERT INTO "usernames" (username, password) VALUES ($1, $2);', [username, password], function(err, result){
+    pool.query('INSERT INTO "usernames" (username, password) VALUES ($1, $2);', [username, dbString], function(err, result){
         if(err){
             res.status(500).send(err.toString());
         }
