a841383f458f6259d06e332ca4d6bf844d72569f
aminavjith <amritha.n@gmail.com>
2016-11-18 00:05:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,4 @@
+//to log in
 var submit1 = document.getElementById('submit-user');
 submit1.onclick = function() {
     console.log('logging in');
@@ -85,32 +86,3 @@ function display2() {
     document.getElementById("logout").style.display = "none";
 }
 
-//to submit comment
-var submit = document.getElementById('submit-comment');
-submit.onclick = function() {
-    alert('submitting comment');
-    var inputComment = document.getElementById('comment');
-    var comment = inputComment.value;
-    //inputName.value = '';
-var request = new XMLHttpRequest();
-request.onreadystatechange = function(){
-    if(request.readyState === XMLHttpRequest.DONE)
-        if (request.status === 200)
-            {
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var splitter = '';
-                var list = '';
-                for (var i = 0; i < names.length; i++ ){
-                    splitter = names[i];
-                    var pairs = splitter.split('||');
-                    list += '<li>' + pairs[0] + '</li>';
-                    list += '<li> @' + pairs[1] + '</li><br>';
-            }
-            var ul = document.getElementById('listing');
-            ul.innerHTML = list;
-        }
-    };
-    request.open('GET','http://aminavjith.imad.hasura-app.io/submit-comment?comment=' + comment + '||' + new Date(), true);
-    request.send(null);
-};
