6b2aaf32127daf79aa30b4b29ea0309c9c89db09
aminavjith <amritha.n@gmail.com>
2016-11-19 18:38:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -200,7 +200,8 @@ app.post('/login', function (req, res){
 //endpoint to check login
 app.get('/check-login', function (req, res) {
    if (req.session && req.session.auth && req.session.auth.userId) {
-       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+       // Load the user object
+       pool.query('SELECT * FROM "username WHERE id = $1', [req.session.auth.userId], function (err, result) {
            if (err) {
               res.status(500).send(err.toString());
            } else {
@@ -211,7 +212,6 @@ app.get('/check-login', function (req, res) {
        res.status(400).send('You are not logged in');
    }
 });
-
 
 //end point to append comments
 /*var comments = [];
