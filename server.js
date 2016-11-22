a5f2b5f2cc95b7d68246345eb1c0ed39cb2c7665
aminavjith <amritha.n@gmail.com>
2016-11-23 00:00:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -213,14 +213,6 @@ app.get('/check-login', function (req, res) {
    }
 });
 
-//end point to append comments
-/*var comments = [];
-app.get('/submit-comment/:articleName', function(req, res){
-    var comment = req.query.comment;
-    comments.push(comment);
-    res.send(JSON.stringify(comments));
-});*/
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
