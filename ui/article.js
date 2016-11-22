590175352fbe9151c2aa455b249ff7920b65f2f8
aminavjith <amritha.n@gmail.com>
2016-11-23 00:58:33 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -173,7 +173,7 @@ submit4.onclick = function() {
         }
     request.open('POST','http://aminavjith.imad.hasura-app.io/submit-comment/' + currentArticle, true);
     request.setRequestHeader('Content-Type', 'application/json');
-    request.send(JSON.stringify({comment: inputComment}));
+    request.send(JSON.stringify({inputComment: inputComment}));
     };
 
 
