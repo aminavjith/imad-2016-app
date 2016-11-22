4ca97da548d2926d9426973b3130265dd0e17240
aminavjith <amritha.n@gmail.com>
2016-11-23 00:49:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -43,9 +43,7 @@ function createArticleTemplate(data) {
         <div class = "header">
       <div id="text1" class="header"> Learning to create webapps
       </div>
-
       <body1 id="login"  >
-
         <div id="userarea" class="header"> Username <br>
             <input placeholder="Username" id="username" type="name"/> <br>
         </div>
@@ -55,7 +53,6 @@ function createArticleTemplate(data) {
             <input type="button" id="submit-user" class="submit1" value="Login"/>
             <input type="button" id="register" class="submit2" value="Register"/>
       </body1>
-
       <body2 id="logout" style="display:none;">
         <div id="userarea" class="header"> You are now signed in. <br>
             <input type="button" id="logout-user" class="submit3" value="Log Out"/>
@@ -68,7 +65,6 @@ function createArticleTemplate(data) {
         <h3>${heading}</h3>
         <br>
         <hr>
-
         ${date.toDateString()}<br>
         ${content}
        <hr>
@@ -79,7 +75,6 @@ function createArticleTemplate(data) {
       <ul id="listing">
       </ul>
       </body>
-
       <script type="text/javascript" src="/ui/article.js">
       </script>
      </html>`;
