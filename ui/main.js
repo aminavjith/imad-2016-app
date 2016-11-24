08f070247c8ecf477c41ce6047cc876a7318a666
aminavjith <amritha.n@gmail.com>
2016-11-24 15:41:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,11 +30,10 @@ function onLoad(){
                     var articleList = request.responseText;
                     console.log((request.responseText));
                     articleList = JSON.parse(articleList);
-                    
                     var list = '';
                     for (var i = 0; i < articleList.length; i++ ){
                         //<a href="/articles/article-one"> HTML </a> <br>
-                        list += `<a href="/articles/articleList[i].title"> articleList[i].heading </a> <br>`;
+                        list += `<a href="/articles/articleList[i].title}"> ${articleList[i].heading} </a> <br>`;
                         }
                     var ul = document.getElementById('articleList');
                     ul.innerHTML = list;
