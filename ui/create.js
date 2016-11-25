ff5ceb4e9fe97e5d38765b1dd5f998ffe33ed683
aminavjith <amritha.n@gmail.com>
2016-11-25 11:04:23 +0530

    [imad-console] Updates ui/create.js

--- ui/create.js
+++ ui/create.js
@@ -20,3 +20,36 @@ function loadLogin() {
     request.open('GET', 'http://aminavjith.imad.hasura-app.io/check-login', true);
     request.send('null');
 }
+
+var submit6 = document.getElementById('save-article');
+submit6.onclick = function(){
+    var heading = document.getElementById('heading').value;
+    var content = document.getElementById('content').value;
+     if(heading === '' || content === ''){
+        alert('Please enter heading and content in orde be able to save this article.');
+    } else {
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function(){
+            if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                alert('Article was succesfully created.');
+                var newHTML =`
+                 <a href="/" text-align="right">Home</a>
+                 <h3>New article was created successfully.</h3>
+                 <br>
+                 <hr>`;
+                var bodyx = document.getElementById('bodyx');
+                bodyx.innerHTML = newHTML;
+            } else {
+                console.log((request.responseText));
+                alert('Very sorry, but we are not able to save the article.');
+            }
+        }
+    };
+    request.open('POST', 'http://aminavjith.imad.hasura-app.io/save-article', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({heading: heading, comment: comment}));
+    request.responseType = 'text';
+}};
+
+
